d6a49f02b65bf5685e8a0a39c1e3222ab59a1288
Christopher Hewett <chewett@hotmail.co.uk>
2016-03-11 23:16:26 +0000

    Improved CNC setup by making functions of common behavior

--- src/Chewett/CacheNCrunch/CNCSetup.php
+++ src/Chewett/CacheNCrunch/CNCSetup.php
@@ -5,6 +5,12 @@ namespace Chewett\CacheNCrunch;
 class CNCSetup {
 
     public static function setupBaseDirs() {
+        $cachePhpConfigDir = self::setupDirectories();
+        $jsFileCachePath = $cachePhpConfigDir . CacheNCrunch::$JS_FILE_CACHE_DETAILS;
+        file_put_contents($jsFileCachePath, self::getStartOfAutoloadFile());
+    }
+
+    private static function setupDirectories() {
         if(!is_dir(CacheNCrunch::getCacheDirectory())) {
             mkdir(CacheNCrunch::getCacheDirectory());
         }
@@ -13,30 +19,20 @@ class CNCSetup {
         if(!is_dir($cachePhpConfigDir)) {
             mkdir($cachePhpConfigDir, 0777, true);
         }
+        return $cachePhpConfigDir;
+    }
 
-        $jsFileCachePath = $cachePhpConfigDir . CacheNCrunch::$JS_FILE_CACHE_DETAILS;
-        $jsFileCachePathFile =
+    private static function getStartOfAutoloadFile() {
+        return
             '<?php ' . PHP_EOL .
             '$JS_FILES = []; ' . PHP_EOL;
-
-        file_put_contents($jsFileCachePath, $jsFileCachePathFile);
     }
 
     public static function storeDataToCacheFile($data) {
-        if(!is_dir(CacheNCrunch::getCacheDirectory())) {
-            mkdir(CacheNCrunch::getCacheDirectory());
-        }
-
-        $cachePhpConfigDir = CacheNCrunch::getCacheDirectory() . CacheNCrunch::$JS_LOADING_FILES;
-        if(!is_dir($cachePhpConfigDir)) {
-            mkdir($cachePhpConfigDir, 0777, true);
-        }
-
+        $cachePhpConfigDir = self::setupDirectories();
         $jsCurrentFileCachePath = $cachePhpConfigDir . CacheNCrunch::$JS_FILE_CACHE_DETAILS;
         $jsCurrentFileCachePath = str_replace("\\", "/", $jsCurrentFileCachePath);
-        $jsFile =
-            '<?php ' . PHP_EOL .
-            '$JS_FILES = []; ' . PHP_EOL;
+        $jsFile = self::getStartOfAutoloadFile();
 
         foreach($data as $scriptName => $dataElement) {
             $jsFile .= '$JS_FILES["'.$scriptName.'"] = ["md5" => "'.$dataElement['md5'].'", "cachePath" => "'.$dataElement['cachePath'].'", "cacheUrl" => "'.$dataElement['cacheUrl'].'"];' . PHP_EOL;
