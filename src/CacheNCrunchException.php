549e3c09460412d6de56a09c86b06c5e6c1bb20b
Christopher Hewett <chewett@hotmail.co.uk>
2018-09-16 00:21:58 +0100

    Added an exception to use

---
+++ src/CacheNCrunchException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Chewett\CacheNCrunch;
+
+/**
+ * Class CacheNCrunchException
+ * Base exception used for this library
+ *
+ * @package Chewett\CacheNCrunch
+ * @author Christopher Hewett <chewett@hotmail.co.uk>
+ */
+class CacheNCrunchException extends \Exception {
+
+}
