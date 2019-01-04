915f47c78f6a2ba5e453895b541d00c4a863bc00
Christopher Hewett <chewett@hotmail.co.uk>
2019-01-04 14:27:30 +0000

    Added more tests to test the recently added features

--- tests/CacheNCrunchTest.php
+++ tests/CacheNCrunchTest.php
@@ -359,5 +359,90 @@ class CacheNCrunchTest extends \PHPUnit_Framework_TestCase {
 
     }
 
+    /**
+     * Test to make sure that when we ask not to serve crunched files, we dont.
+     * @dataProvider headerFileProvider
+     */
+    public function testDontServeCrunchedFiles($jsHeaderFile, $cssHeaderFile) {
+        if($jsHeaderFile) {
+            $this->cnc->setUglifyJsHeaderFile($jsHeaderFile);
+        }
+        if($cssHeaderFile) {
+            $this->cnc->setUglifyCssHeaderFile($cssHeaderFile);
+        }
+        $this->cnc->setDontServeCrunchedFiles(true);
+        $this->cnc->registerJsFile("testJs", "/static/testJs.js", __DIR__ . "/../static/testJs.js");
+        $this->cnc->registerCssFile("testCss", "/static/testCss.css", __DIR__ . "/../static/testCss.css");
+        $this->cnc->crunch();
+
+        $this->assertEquals(
+            "<link href='/static/testCss.css' rel='stylesheet'><script src='/static/testJs.js'></script>",
+            $this->cnc->getScriptImports()
+        );
+    }
+
+    /**
+     * Get script imports in debug mode, Then turn off debug mode and get it again.
+     *
+     * If crunching in debug mode is turned on, this should have crunched already, and not in debug mode should load the correct data
+     * @dataProvider headerFileProvider
+     */
+    public function testDebugModeHasCrunched($jsHeaderFile, $cssHeaderFile) {
+        if($jsHeaderFile) {
+            $this->cnc->setUglifyJsHeaderFile($jsHeaderFile);
+        }
+        if($cssHeaderFile) {
+            $this->cnc->setUglifyCssHeaderFile($cssHeaderFile);
+        }
+        $this->cnc->setCrunchAlwaysOnDevMode(true);
+        $this->cnc->setDebugMode(true);
+        $this->cnc->registerJsFile("testJs", "/static/testJs.js", __DIR__ . "/../static/testJs.js");
+        $this->cnc->registerCssFile("testCss", "/static/testCss.css", __DIR__ . "/../static/testCss.css");
+        $this->assertEquals(
+            "<link href='/static/testCss.css' rel='stylesheet'><script src='/static/testJs.js'></script>",
+            $this->cnc->getScriptImports()
+        );
+        $this->cnc->setDebugMode(false);
+
+        $importStatements = $this->cnc->getScriptImports();
+
+        //Make sure there is only one import for each type
+        $this->assertEquals(1, substr_count($importStatements, "script src="));
+        $this->assertEquals(1, substr_count($importStatements, "link href="));
+        //Make sure its not importing the old files directly
+        $this->assertEquals(0, substr_count($importStatements, "testJs.js"));
+        $this->assertEquals(0, substr_count($importStatements, "testCss.css"));
+    }
+
+    /**
+     * Get script imports in debug mode, Then turn off debug mode and get it again.
+     *
+     * This shouldnt crunch in debug mode, so it should correctly still return the specific imports
+     * @dataProvider headerFileProvider
+     */
+    public function testDebugModeHasNotCrunched($jsHeaderFile, $cssHeaderFile) {
+        if ($jsHeaderFile) {
+            $this->cnc->setUglifyJsHeaderFile($jsHeaderFile);
+        }
+        if ($cssHeaderFile) {
+            $this->cnc->setUglifyCssHeaderFile($cssHeaderFile);
+        }
+        $this->cnc->setCrunchAlwaysOnDevMode(false);
+        $this->cnc->setDebugMode(true);
+        $this->cnc->registerJsFile("testJs", "/static/testJs.js", __DIR__ . "/../static/testJs.js");
+        $this->cnc->registerCssFile("testCss", "/static/testCss.css", __DIR__ . "/../static/testCss.css");
+        $this->assertEquals(
+            "<link href='/static/testCss.css' rel='stylesheet'><script src='/static/testJs.js'></script>",
+            $this->cnc->getScriptImports()
+        );
+        $this->cnc->setDebugMode(false);
+
+        //Make sure that this still hasnt crushed the files
+        $this->assertEquals(
+            "<link href='/static/testCss.css' rel='stylesheet'><script src='/static/testJs.js'></script>",
+            $this->cnc->getScriptImports()
+        );
+    }
+
 
 }
